3b39c7ad7612579be100259900d91ac011dca209
philo <philopodickens92@gmail.com>
2021-07-26 12:53:57 +0300

    change names of ids in scripts.js, delete some ids in p and addd edits on index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,99 @@
 
 $(document).ready(function(){
-    $("#deve-image").click(function(){
-      $("#deve-image").slideDown('1500').hide('1000');
-      $("#deve").show('1500');
-    });
-    $("#deve").click(function(){
-      $("#deve").slideUp('1500');
-      $("#deve-image").slideDown('1500');
-    });
+  $("#deve-image").click(function(){
+    $("#deve-image").slideDown('1500').hide('1000');
+    $("#devep").show('1500');
   });
-  
-  $(document).ready(function(){
-    $("#des-image").click(function(){
-      $("#des-image").slideDown('1500').hide('1000');
-      $("#des").show('1500');
-    });
-    $("#des").click(function(){
-      $("#des").slideUp('1500');
-      $("#des-image").slideDown('1500');
-    });
+  $("#devep").click(function(){
+    $("#devep").slideUp('1500');
+    $("#deve-image").slideDown('1500');
   });
-  
-  $(document).ready(function(){
-    $("#pmanagement-image").click(function(){
-      $("#pmanagement-image").slideDown('1500').hide('1000');
-      $("#pmanagement").show('1500');
-    });
-    $("#pmanagement").click(function(){
-      $("#pmanagement").slideUp('1500');
-      $("#pmanagement-image").slideDown('1500');
-    });
+});
+
+$(document).ready(function(){
+  $("#des-image").click(function(){
+    $("#des-image").slideDown('1500').hide('1000');
+    $("#desp").show('1500');
   });
-  $(document).ready(function(){
-    $("#work1").mouseover(function(){
+  $("#desp").click(function(){
+    $("#desp").slideUp('1500');
+    $("#des-image").slideDown('1500');
+  });
+});
+
+$(document).ready(function(){
+  $("#pmanagement-image").click(function(){
+    $("#pmanagement-image").slideDown('1500').hide('1000');
+    $("#PMp").show('1500');
+  });
+  $("#PMp").click(function(){
+    $("#PMp").slideUp('1500');
+    $("#pmanagement-image").slideDown('1500');
+  });
+});
+  $(document).ready(function () {
+    $("#work1").mouseover(function () {
       $("#overlay1").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
       $("#overlay1").hide();
     });
   });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
+
+  $(document).ready(function () {
+    $("#work2").mouseover(function () {
       $("#overlay2").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
       $("#overlay2").hide();
     });
   });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
+  $(document).ready(function () {
+    $("#work3").mouseover(function () {
       $("#overlay3").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
       $("#overlay3").hide();
     });
   });
-  $(document).ready(function(){
-    $("#work4").mouseover(function(){
+  $(document).ready(function () {
+    $("#work4").mouseover(function () {
       $("#overlay4").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
       $("#overlay4").hide();
     });
   });
-  
-  $(document).ready(function(){
-    $("#work5").mouseover(function(){
+
+  $(document).ready(function () {
+    $("#work5").mouseover(function () {
       $("#overlay5").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
       $("#overlay5").hide();
     });
-    $("#work6").mouseover(function(){
+    $("#work6").mouseover(function () {
       $("#overlay6").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
       $("#overlay6").hide();
     });
-    $("#work7").mouseover(function(){
+    $("#work7").mouseover(function () {
       $("#overlay7").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
       $("#overlay7").hide();
     });
-    $("#work8").mouseover(function(){
+    $("#work8").mouseover(function () {
       $("#overlay8").show();
-    }).mouseout(function(){
+    }).mouseout(function () {
       $("#overlay8").hide();
     });
   });
-  $("form#form").on('submit',function(event){
+  $("form#form").on('submit', function (event) {
     event.preventDefault();
     let name = $("input#name").val();
     let email = $("input#email").val();
     let message = $("textarea#comment").val();
 
-    if ($("input#name").val() && $("input#email").val()){
-        alert ("Hello " + name + ", We appreciate you for reaching out");
+    if ($("input#name").val() && $("input#email").val()) {
+      alert("Hello " + name + ", We appreciate you for reaching out");
     }
     else {
-        alert("Kindly enter valid name and emailPlease provide your correct name and email!");
+      alert("Kindly enter valid name and email address!");
     }
 
-});
+  });
